2ecfde73984ad1091cc1d4b4c508c166cd8a6354
beamalsky <beamalsky@gmail.com>
2020-01-24 12:50:59 -0600

    Remove distribution archive files from version control

--- build/lib/django_geomultiplechoice/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class DjangoGeomultiplechoiceConfig(AppConfig):
-    name = 'django_geomultiplechoice'
