175943e72ded7b96067cf6687499d3630415823b
beamalsky <beamalsky@gmail.com>
2020-01-27 11:46:27 -0600

    Exclude example repo, link to full README on pypi

---
+++ README_pypi.md
@@ -0,0 +1,9 @@
+# 🗺 GeoMultipleChoice
+
+A Django widget to select multiple geographic areas.
+
+![GeoMultipleChoice demo](https://raw.githubusercontent.com/datamade/django-geomultiplechoice/master/images/just-spaces.gif)
+
+Originally created by [@jeancochrane](https://github.com/jeancochrane). Packaged up by [@beamalsky](https://github.com/beamalsky). Both from [DataMade](https://datamade.us/).
+
+**For documentation of this widget, including an example implementation and guidelines for local development, view [the django-geomultiplechoice GitHub repo](https://github.com/datamade/django-geomultiplechoice).**
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README_pypi.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="django-geomultiplechoice",
-    version="0.0.3",
+    version="0.0.4",
     author="Bea Malsky",
     author_email="beamalsky@datamade.us",
     description="A Django widget to select multiple geographic areas",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/datamade//django-geomultiplechoice",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['django_geomultiplechoice']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
